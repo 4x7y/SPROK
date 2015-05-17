3d6e7b38037e9a9f519cdb86af7bef77548e1706
WestDan <pb12203148@gmail.com>
2015-05-17 21:14:21 +0800

    PCL include file move to ROS node talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -34,6 +34,91 @@
 
 #include <sstream>
 
+
+// PCL include file
+#include <pcl/io/openni_grabber.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/console/parse.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace pcl;
+
+PointCloud<PointXYZRGBA>::Ptr cloudptr(new PointCloud<PointXYZRGBA>); // A cloud that will store color info.
+PointCloud<PointXYZ>::Ptr fallbackCloud(new PointCloud<PointXYZ>);    // A fallback cloud with just depth data.
+boost::shared_ptr<visualization::CloudViewer> viewer;                 // Point cloud viewer object.
+Grabber* openniGrabber;                                               // OpenNI grabber that takes data from the device.
+unsigned int filesSaved = 0;                                          // For the numbering of the clouds saved to disk.
+bool saveCloud(false), noColor(false);                                // Program control..
+
+void
+printUsage(const char* programName)
+{
+        cout << "Usage: " << programName << " [options]"
+                 << endl
+                 << endl
+                 << "Options:\n"
+                 << endl
+                 << "\t<none>     start capturing from an OpenNI device.\n"
+                 << "\t-v FILE    visualize the given .pcd file.\n"
+                 << "\t-h         shows this help.\n";
+}
+
+// This function is called every time the device has new data.
+void
+grabberCallback(const PointCloud<PointXYZRGBA>::ConstPtr& cloud)
+{
+        if (! viewer->wasStopped())
+                viewer->showCloud(cloud);
+
+        if (saveCloud)
+        {
+                stringstream stream;
+                stream << "inputCloud" << filesSaved << ".pcd";
+                string filename = stream.str();
+                if (io::savePCDFileASCII(filename, *cloud) == 0)
+                {
+                        filesSaved++;
+                        cout << "Saved " << filename << "." << endl;
+                }
+                else PCL_ERROR("Problem saving %s.\n", filename.c_str());
+
+                saveCloud = false;
+        }
+}
+
+// For detecting when SPACE is pressed.
+void
+keyboardEventOccurred(const visualization::KeyboardEvent& event,
+                                          void* nothing)
+{
+        if (event.getKeySym() == "space" && event.keyDown())
+                saveCloud = true;
+}
+
+// Creates, initializes and returns a new viewer.
+boost::shared_ptr<visualization::CloudViewer>
+createViewer()
+{
+        boost::shared_ptr<visualization::CloudViewer> v
+        (new visualization::CloudViewer("OpenNI viewer"));
+        v->registerKeyboardCallback(keyboardEventOccurred);
+
+        return (v);
+}
+
+
+
+
+
+
+
+
+
+
+
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
